bf15101c6bbdf62428e808084a1a28e0631e7296
struuuuggle <s15ti032@gmail.com>
2017-11-08 00:07:52 +0900

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ chmod a+x grl
 # 'getLinkFormat.py'のフルパス
 abspath=`find \`pwd\` -name "getLinkFormat.py"`
 
+# grlコマンドファイルを生成
+touch grl
+
 # grlに追記
 echo "#!/bin/sh\n\n# set command from setup.sh\npython $abspath" > grl
 
